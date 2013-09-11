ffe6f025fd0be1f229bec7d1db2af10c5a1ec5fe
Tim Caswell <tim@creationix.com>
2013-09-11 12:06:33 -0500

    Add uv.close calls to test-spawn.lua

--- examples/test-spawn.lua
+++ examples/test-spawn.lua
@@ -12,6 +12,8 @@ local handle, stdin, stdout, stderr, pid = uv.spawn("sleep", {"100"})
 p(handle, {stdin=stdin,stdout=stdout,stderr=stderr, pid=pid})
 function handle:onexit(code, signal)
   p("EXIT", {code=code,signal=signal})
+  uv.close(handle)
+  uv.close(stdin)
 end
 function stdout:ondata(chunk)
   p("STDOUT.DATA", chunk)
@@ -21,9 +23,11 @@ function stderr:ondata(chunk)
 end
 function stdout:onend()
   p("STDOUT.END")
+  uv.close(stdout)
 end
 function stderr:onend()
   p("STDERR.END")
+  uv.close(stderr)
 end
 uv.read_start(stderr)
 uv.read_start(stdout)
