57f563fc610f7056af68cb6729e9a27fad3ad035
Tim Caswell <tim@creationix.com>
2014-09-08 16:20:00 -0500

    += instead of = for CFLAGS and LDFLAGS

--- Makefile.lua
+++ Makefile.lua
@@ -1,14 +1,14 @@
-CFLAGS=-Ilibuv/include -g -I/usr/local/include \
+CFLAGS+=-Ilibuv/include -g -I/usr/local/include \
 	-DLUV_STACK_CHECK -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 \
 	-Wall -Werror -fPIC
 
 uname_S=$(shell uname -s)
 ifeq (Darwin, $(uname_S))
-	LDFLAGS=-bundle -bundle_loader /usr/local/bin/lua \
+	LDFLAGS+=-bundle -bundle_loader /usr/local/bin/lua \
 		-framework CoreServices -framework Cocoa \
 		-L/usr/local/lib/
 else
-	LDFLAGS=-shared -lrt
+	LDFLAGS+=-shared -lrt
 endif
 
 SOURCE_FILES=\
