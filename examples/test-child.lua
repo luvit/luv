16327851e3369d5feeca4be5dcf4ed2e931097eb
Tim Caswell <tim@creationix.com>
2014-09-30 10:29:33 -0500

    Update spawn examples

--- examples/test-child.lua
+++ examples/test-child.lua
@@ -1,34 +1,44 @@
 local p = require('lib/utils').prettyPrint
 local uv = require('luv')
+local loop = assert(uv.new_loop())
 
 -- The parent is going to pass us the server handle over a pipe
 -- This will be our local file descriptor at PIPE_FD
-local pipe = uv.new_pipe(true)
+local pipe = uv.new_pipe(loop, true)
 uv.pipe_open(pipe, tonumber(os.getenv("PIPE_FD")))
 
 
 -- Configure the server handle
-local server = uv.new_tcp()
+local server = uv.new_tcp(loop)
 function server:onconnection()
-  local client = uv.new_tcp()
+  local client = uv.new_tcp(loop)
   uv.accept(server, client)
   p("New TCP", client, "on", server)
   p{client=client}
-  uv.write(client, "BYE!\n");
+  uv.write(uv.write_req(), client, "BYE!\n");
   uv.shutdown(client, function ()
     uv.close(client)
   end)
 end
 
 -- Read the server handle from the parent
-function pipe:ondata(data, pending)
-  if pending then
+function pipe:onread(err, data)
+  if err then error(err) end
+  if uv.pipe_pending_count(self) then
+    local pending_type = uv.pipe_pending_type(self)
+    p("pending_type", pending_type)
     uv.accept(pipe, server)
     uv.listen(server)
     p("Received server handle from parent process", server)
+  elseif data then
+    p("ondata", data)
+  else
+    p("onend", data)
+  end
+  if pending then
   end
 end
-uv.read2_start(pipe)
+uv.read_start(pipe)
 
 -- Start the event loop!
-uv.run("default")
+uv.run(loop)
--- examples/test-master.lua
+++ examples/test-master.lua
@@ -1,25 +1,29 @@
 local p = require('lib/utils').prettyPrint
 local uv = require('luv')
+local loop = assert(uv.new_loop())
 
-local server = uv.new_tcp()
+local execpath = "/usr/local/bin/luajit" -- uv.execpath()
+local cpu_count = 4 -- #uv.cpu_info()
+
+local server = uv.new_tcp(loop)
 uv.tcp_bind(server, "127.0.0.1", 1337)
 print("Master process bound to TCP port 1337 on 127.0.0.1")
 
 local function spawnChild()
-  local pipe = uv.new_pipe(true)
-  local child, pid = uv.spawn(uv.execpath(), {"examples/test-child.lua"},{
+  local pipe = uv.new_pipe(loop, true)
+  local child, pid = uv.spawn(loop, execpath, {"examples/test-child.lua"},{
     stdio= {nil,1,2,pipe},
     env= {"PIPE_FD=3"}
   })
   p("Spawned child", pid, "and sending handle", server)
-  uv.write2(pipe, "123", server)
-  uv.shutdown(pipe)
+  uv.write2(uv.write_req(), pipe, "123", server)
+  uv.shutdown(uv.shutdown_req(), pipe)
 end
 
 -- Spawn a child process for each CPU core
-for i = 1, #uv.cpu_info() do
+for i = 1, cpu_count do
   spawnChild()
 end
 
-uv.run("default")
+uv.run(loop)
 
