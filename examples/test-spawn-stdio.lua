3c4bc57afd58cc8e38c6a3ac975ff52737da7491
Tim Caswell <tim@creationix.com>
2014-10-02 17:57:32 -0500

    Fix stdio example

--- examples/test-spawn-stdio.lua
+++ examples/test-spawn-stdio.lua
@@ -1,51 +1,55 @@
 local p = require('lib/utils').prettyPrint
 local uv = require('luv')
-local loop = assert(uv.new_loop())
 
-local stdout = uv.new_pipe(loop, false)
-local stderr = uv.new_pipe(loop, false)
-local stdin = uv.new_pipe(loop, false)
+local stdout = uv.new_pipe(false)
+local stderr = uv.new_pipe( false)
+local stdin = uv.new_pipe(false)
 
-local handle, pid = uv.spawn(loop, "cat", {}, {
+
+local function onexit(self, code, signal)
+  p("exit", {self=self, code=code,signal=signal})
+end
+
+local function onclose(self)
+  p("close", {self=self})
+end
+
+local function onread(self, err, chunk)
+  assert(not err, err)
+  if (chunk) then
+    p("data", {self=self,data=chunk})
+  else
+    p("end", {self=self})
+  end
+end
+
+local function onclose(self)
+  p("onclose", {self=self})
+end
+
+local function onshutdown(self)
+  uv.close(self, onclose)
+end
+
+local handle, pid = uv.spawn("cat", {
   stdio = {stdin, stdout, stderr}
-})
+}, onexit)
 
 p{
   handle=handle,
   pid=pid
 }
 
-function handle:onexit(code, signal)
-  p("exit", {code=code,signal=signal})
-end
+uv.read_start(stdout, onread)
+uv.read_start(stderr, onread)
+uv.write(stdin, "Hello World")
+uv.shutdown(stdin, onshutdown)
 
-function handle:onclose()
-  p("handle close")
-end
+uv.run(loop)
 
-function stdout:onread(err, chunk)
-  if err then error(err) end
-  if (chunk) then
-    p("stdout data", chunk)
-  else
-    p("stdout end")
-  end
-end
-
-function stderr:onread(err, chunk)
-  if err then error(err) end
-  if (chunk) then
-    p("stderr data", chunk)
-  else
-    p("stderr end")
-  end
-end
-
-uv.read_start(stdout)
-uv.read_start(stderr)
-uv.write(uv.write_req(), stdin, "Hello World")
-uv.shutdown(uv.shutdown_req(), stdin, function ()
-  uv.close(stdin)
+-- Close everything
+uv.walk(function (handle)
+  uv.close(handle, onclose)
 end)
 
 uv.run(loop)
