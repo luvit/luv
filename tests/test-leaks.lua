f006bdf5481c54d67410488412fee2e3348249e7
Tim Caswell <tim@creationix.com>
2014-10-13 11:56:38 -0500

    Make leak test less sensitive

--- tests/test-leaks.lua
+++ tests/test-leaks.lua
@@ -7,7 +7,7 @@ return require('lib/tap')(function (test)
     for i = 1, 0x10000 do
       timer = uv.new_timer()
       uv.close(timer)
-      if i % 0x1000 == 0 then
+      if i % 0x2000 == 0 then
         timer = nil
         uv.run()
         collectgarbage()
@@ -23,7 +23,7 @@ return require('lib/tap')(function (test)
       before = before,
       after = after,
     }
-    assert(after < before * 1.1, "Leak in timers")
+    assert(after < before * 1.5, "Leak in timers")
   end)
 
 end)
