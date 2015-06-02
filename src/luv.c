fce4cbdc403b164fd891ed5b8299ab78f82f5db2
runner.mei <runner.mei@gmail.com>
2015-06-02 19:32:15 +0800

    add new_socket_poll method

--- src/luv.c
+++ src/luv.c
@@ -99,6 +99,7 @@ static const luaL_Reg luv_functions[] = {
 
   // poll.c
   {"new_poll", luv_new_poll},
+  {"new_socket_poll", luv_new_socket_poll},
   {"poll_start", luv_poll_start},
   {"poll_stop", luv_poll_stop},
 
