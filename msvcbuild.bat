eae6cddad599b7669939c5db023edc735ae3cb94
Thiago Bastos <tbastos@users.noreply.github.com>
2016-01-22 12:57:41 +0100

    Fix AppVeyor and Travis

--- msvcbuild.bat
+++ msvcbuild.bat
@@ -1,9 +1,11 @@
-set ARCH= Win64"
-if "%platform%" EQU "x86" (set ARCH=")
+@echo off
 
-set VS_VER="12"
-if "%configuration%"=="2015" ( set VS_VER="14" )
-if "%configuration%"=="2013" ( set VS_VER="12" )
+set ARCH= Win64^"
+if "%platform%" EQU "x86" (set ARCH=^")
+
+set VS_VER=12
+if "%configuration%"=="2015" ( set VS_VER=14 )
+if "%configuration%"=="2013" ( set VS_VER=12 )
 
 cmake -H. -Bbuild -G"Visual Studio %VS_VER%%ARCH%
 cmake --build build --config Release
