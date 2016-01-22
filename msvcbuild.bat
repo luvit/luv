b1bf97385193c154d53b2840f73fb30bab55dd2c
Thiago Bastos <tbastos@users.noreply.github.com>
2016-01-22 12:03:54 +0100

    Fix AppVeyor and Travis

--- msvcbuild.bat
+++ msvcbuild.bat
@@ -1,11 +1,11 @@
-set ARCH=" Win64"
-if "%platform%" EQU "x86" ( set ARCH="" )
+set ARCH= Win64"
+if "%platform%" EQU "x86" (set ARCH=")
 
 set VS_VER="12"
 if "%configuration%"=="2015" ( set VS_VER="14" )
 if "%configuration%"=="2013" ( set VS_VER="12" )
 
-cmake -H. -Bbuild -G"Visual Studio %VS_VER%%ARCH%"
+cmake -H. -Bbuild -G"Visual Studio %VS_VER%%ARCH%
 cmake --build build --config Release
 copy build\Release\luv.dll .
 copy build\Release\luajit.exe .
