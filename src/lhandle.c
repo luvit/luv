luvit/luv

commit e5ec37cd6c16c963ce9158cf0b422a6276c030a9

fix error handle in callback with lua_pcall according to https://www.lua.org/manual/5.1/manual.html#lua_pcall, errfunc cannot be a pseudo-index.

src/lhandle.c
@@ -87,8 +87,11 @@ static void luv_call_callback(lua_State* L, luv_handle_t* data, luv_callback_id 
     lua_pop(L, nargs);
   }
   else {
+    int errfunc = 0;
+
     // Get the traceback function in case of error
     lua_pushcfunction(L, traceback);
+    errfunc = lua_gettop(L);
     // And insert it before the args if there are any.
     if (nargs) {
       lua_insert(L, -1 - nargs);
@@ -100,7 +103,7 @@ static void luv_call_callback(lua_State* L, luv_handle_t* data, luv_callback_id 
       lua_insert(L, -1 - nargs);
     }
 
-    if (lua_pcall(L, nargs, 0, -2 - nargs)) {
+    if (lua_pcall(L, nargs, 0, errfunc)) {
       fprintf(stderr, "Uncaught Error: %s\n", lua_tostring(L, -1));
       exit(-1);
     }

commit d3319b57e0621780bcd92045ecd59c2df3b2a726

fix bug with wrong errfunc index, caused by https://github.com/luvit/luv/commit/e5ec37cd6c16c963ce9158cf0b422a6276c030a9

src/lhandle.c
@@ -95,6 +95,7 @@ static void luv_call_callback(lua_State* L, luv_handle_t* data, luv_callback_id 
     // And insert it before the args if there are any.
     if (nargs) {
       lua_insert(L, -1 - nargs);
+      errfunc -= nargs;
     }
     // Get the callback
     lua_rawgeti(L, LUA_REGISTRYINDEX, ref);