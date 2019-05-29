c883f68513e5dafd377597be721036694a3709ad
George Zhao <zhaozg@gmail.com>
2019-05-29 23:16:06 +0800

    try to support extern uv_loop_t

--- src/luv.h
+++ src/luv.h
@@ -54,15 +54,22 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-function"
 #endif
+
 /* There is a 1-1 relation between a lua_State and a uv_loop_t
    These helpers will give you one if you have the other
    These are exposed for extensions built with luv
    This allows luv to be used in multithreaded applications.
 */
 LUALIB_API lua_State* luv_state(uv_loop_t* loop);
-/* All libuv callbacks will lua_call directly from this root-per-thread state
-*/
+
+/* All libuv callbacks will lua_call directly from this root-per-thread state */
 LUALIB_API uv_loop_t* luv_loop(lua_State* L);
+
+/* Set an extern uv_loop_t in a lua_State
+   This must be called before luaopen_luv, luaopen_luv don't init an inner loop
+   When loop is NULL, will clear previous extern loop
+*/
+LUALIB_API void luv_set_loop(lua_State* L, uv_loop_t* loop);
 
 /* This is the main hook to load the library.
    This can be called multiple times in a process as long
