7003dde7e7c83ed727a792cfd73d818dea6e1f97
Ryan Liptak <squeek502@hotmail.com>
2019-10-09 03:06:48 -0700

    More experimenting

--- msvcbuild.bat
+++ msvcbuild.bat
@@ -1,9 +1,10 @@
 @echo off
 
-set VS=14
-if "%configuration%"=="2017" (set VS=15)
-if "%configuration%"=="2015" (set VS=14)
-if "%configuration%"=="2013" (set VS=12)
+if not defined VS set VS=14
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (set VS=16)
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (set VS=15)
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (set VS=14)
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2013" (set VS=12)
 
 if not defined platform set platform=x64
 if "%platform%" EQU "x64" (set VS=%VS% Win64)
