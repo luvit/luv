f781e85f9f9032fb2db09e32039272bc9635b624
Ryan Liptak <squeek502@hotmail.com>
2019-10-11 03:00:14 -0700

    Some msvcbuild.bat improvements

--- msvcbuild.bat
+++ msvcbuild.bat
@@ -1,4 +1,5 @@
 @echo off
+Setlocal EnableDelayedExpansion
 
 if not defined VS set VS=15
 if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (set VS=16)
@@ -9,8 +10,9 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2013" (set VS=12)
 set ARCH=
 if %VS% LEQ 15 (
 	if not defined platform set platform=x64
-	if "%platform%" EQU "x64" (set VS=%VS% Win64)
+	if "!platform!" EQU "x64" (set VS=%VS% Win64)
 ) else (
+	:: If -A is not specified, defaults to host arch
 	if "%platform%" EQU "x86" (set ARCH= -A Win32)
 	if "%platform%" EQU "x64" (set ARCH= -A x64)
 )
