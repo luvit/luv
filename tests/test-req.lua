5c58bfca0e124f4e24ef32b140c218d2fcb62cfd
Ryan Liptak <squeek502@hotmail.com>
2020-05-11 21:42:32 -0700

    test-req: Add cancel test

--- tests/test-req.lua
+++ tests/test-req.lua
@@ -1,5 +1,23 @@
 return require('lib/tap')(function (test)
 
+  test("cancel", function (print, p, expect, uv)
+    -- might need to try a few times since cancel can fail with EBUSY
+    for _=1,5 do
+      local req = assert(uv.fs_stat('.', expect(function(err)
+        if err then
+          assert(err:match("^ECANCELED"))
+        else
+          assert(not err, err)
+        end
+      end)))
+
+      local _, cancel_err = req:cancel()
+      if cancel_err == nil then
+        break
+      end
+    end
+  end)
+
   test("get type", function (print, p, expect, uv)
     local req = uv.fs_stat('.', expect(function(err)
       assert(not err, err)
