7877a2280e6a9232e280e82e59d0541eb28a5729
Ryan Liptak <squeek502@hotmail.com>
2024-01-17 09:44:51 -0800

    utils: Use os_uname when it's available to detect Linux

--- lib/utils.lua
+++ lib/utils.lua
@@ -12,6 +12,20 @@ else
   usecolors = false
 end
 
+function utils.uvVersionGEQ(min_version)
+  if not min_version then return true end
+  local min_version_num = min_version
+  if type(min_version) == "string" then
+    local version_parts = {}
+    for part in min_version:gmatch("%d+") do
+      table.insert(version_parts, tonumber(part))
+    end
+    assert(#version_parts == 3, "malformed version string: " .. min_version)
+    min_version_num = version_parts[1]*0x10000 + version_parts[2]*0x100 + version_parts[3]
+  end
+  return uv.version() >= min_version_num
+end
+
 if _G.jit and _G.jit.os then
   -- Luajit provides explicit platform detection
   utils.isWindows = _G.jit.os == "Windows"
@@ -22,12 +36,17 @@ else
   utils.isLinux = false
 
   if not utils.isWindows then
-    -- TODO Use uv.os_uname() when the minimum required libuv provides it
-    local popen_handle = io.popen('uname -s')
-    if popen_handle then
-      local uname_os = assert(popen_handle:read('*a'))
-      popen_handle:close()
-      utils.isLinux = uname_os:lower() == 'linux'
+    -- Use os_uname if it's available, fallback to popen
+    if utils.uvVersionGEQ("1.25.0") then
+      local uname = uv.os_uname()
+      utils.isLinux = uname.sysname:lower() == 'linux'
+    else
+      local popen_handle = io.popen('uname -s')
+      if popen_handle then
+        local uname_os = assert(popen_handle:read('*a'))
+        popen_handle:close()
+        utils.isLinux = uname_os:lower() == 'linux'
+      end
     end
   end
 end
@@ -179,20 +198,6 @@ function utils.prettyPrint(...)
   end
 
   print(table.concat(arguments, "\t"))
-end
-
-function utils.uvVersionGEQ(min_version)
-  if not min_version then return true end
-  local min_version_num = min_version
-  if type(min_version) == "string" then
-    local version_parts = {}
-    for part in min_version:gmatch("%d+") do
-      table.insert(version_parts, tonumber(part))
-    end
-    assert(#version_parts == 3, "malformed version string: " .. min_version)
-    min_version_num = version_parts[1]*0x10000 + version_parts[2]*0x100 + version_parts[3]
-  end
-  return uv.version() >= min_version_num
 end
 
 return utils
