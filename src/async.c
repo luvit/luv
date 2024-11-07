bde4fbc10db23d92bc3a7d9b32ddcdc917a6281b
javalikescript <javalikescript@free.fr>
2024-11-07 22:11:14 +0100

    Move clear to GC and use mutex for arg

--- src/async.c
+++ src/async.c
@@ -16,6 +16,12 @@
  */
 #include "private.h"
 
+typedef struct {
+  luv_thread_arg_t targ;
+  uv_mutex_t mutex; // protect thread arg modifications
+  lua_State* L; // sender Lua state or NULL when nothing to send
+} luv_async_arg_t;
+
 static uv_async_t* luv_check_async(lua_State* L, int index) {
   uv_async_t* handle = (uv_async_t*)luv_checkudata(L, index, "uv_async");
   luaL_argcheck(L, handle->type == UV_ASYNC && handle->data, index, "Expected uv_async_t");
@@ -25,9 +31,19 @@ static uv_async_t* luv_check_async(lua_State* L, int index) {
 static void luv_async_cb(uv_async_t* handle) {
   luv_handle_t* data = (luv_handle_t*)handle->data;
   lua_State* L = data->ctx->L;
-  int n = luv_thread_arg_push(L, (luv_thread_arg_t*)data->extra, LUVF_THREAD_SIDE_MAIN);
-  luv_call_callback(L, data, LUV_ASYNC, n);
-  luv_thread_arg_clear(L, (luv_thread_arg_t*)data->extra, LUVF_THREAD_SIDE_MAIN);
+  luv_async_arg_t* asarg = (luv_async_arg_t *)((luv_handle_t*) handle->data)->extra;
+  uv_mutex_t *argmutex = &asarg->mutex;
+  int n = -1;
+  uv_mutex_lock(argmutex);
+  if (asarg->L) {
+    n = luv_thread_arg_push(L, &asarg->targ, LUVF_THREAD_SIDE_MAIN);
+    luv_thread_arg_clear(L, &asarg->targ, LUVF_THREAD_SIDE_MAIN);
+    asarg->L = NULL;
+  }
+  uv_mutex_unlock(argmutex);
+  if (n >= 0) {
+    luv_call_callback(L, data, LUV_ASYNC, n);
+  }
 }
 
 static int luv_new_async(lua_State* L) {
@@ -52,9 +68,14 @@ static int luv_new_async(lua_State* L) {
   }
   ref->count = 1;
   data = luv_setup_handle(L, ctx);
-  data->extra = (luv_thread_arg_t*)malloc(sizeof(luv_thread_arg_t));
+  luv_async_arg_t* asarg = (luv_async_arg_t*)malloc(sizeof(luv_async_arg_t));
+  data->extra = asarg;
   data->extra_gc = free;
-  memset(data->extra, 0, sizeof(luv_thread_arg_t));
+  memset(asarg, 0, sizeof(luv_async_arg_t));
+  ret = uv_mutex_init(&asarg->mutex);
+  if (ret < 0) { // unlikely
+    abort();
+  }
   handle->data = data;
   luv_check_callback(L, (luv_handle_t*)handle->data, LUV_ASYNC, 1);
   return 1;
@@ -63,29 +84,58 @@ static int luv_new_async(lua_State* L) {
 static int luv_handle_gc(lua_State* L);
 
 static int luv_async_gc(lua_State* L) {
-  luv_ref_t** udata = (luv_ref_t**)lua_touserdata(L, 1);
-  luv_ref_t* ref = *udata;
-  uv_mutex_t *mutex = &ref->mutex;
+  luv_ref_t* ref = *(luv_ref_t**)lua_touserdata(L, 1);
+  uv_mutex_t *refmutex = &ref->mutex;
+  luv_handle_t* data = (luv_handle_t*)ref->handle.async.data;
+  luv_async_arg_t* asarg = (luv_async_arg_t *)data->extra;
+  uv_mutex_t *argmutex = &asarg->mutex;
   int count;
-  uv_mutex_lock(mutex);
+  // clear arg if we are the sender
+  uv_mutex_lock(argmutex);
+  if (asarg->L == L) {
+    luv_thread_arg_clear(L, &asarg->targ, LUVF_THREAD_SIDE_CHILD);
+    asarg->L = NULL;
+  }
+  uv_mutex_unlock(argmutex);
+  // decrease reference count
+  uv_mutex_lock(refmutex);
   ref->count--;
   count = ref->count;
-  uv_mutex_unlock(mutex);
+  uv_mutex_unlock(refmutex);
   if (count > 0) {
     return 0;
   }
-  uv_mutex_destroy(mutex);
+  uv_mutex_destroy(argmutex);
+  uv_mutex_destroy(refmutex);
   return luv_handle_gc(L);
 }
 
 static int luv_async_send(lua_State* L) {
   int ret;
   uv_async_t* handle = luv_check_async(L, 1);
-  luv_thread_arg_t* arg = (luv_thread_arg_t *)((luv_handle_t*) handle->data)->extra;
-
-  luv_thread_arg_set(L, arg, 2, lua_gettop(L), LUVF_THREAD_MODE_ASYNC|LUVF_THREAD_SIDE_CHILD);
+  luv_async_arg_t* asarg = (luv_async_arg_t *)((luv_handle_t*) handle->data)->extra;
+  uv_mutex_t *argmutex = &asarg->mutex;
+  int n;
+  uv_mutex_lock(argmutex);
+  // clear pending arg if any and if owner
+  if (asarg->L) {
+    if (asarg->L != L) {
+      uv_mutex_unlock(argmutex);
+      return luaL_error(L, "Unable to clear the pending send not a the current Lua state");
+    }
+    luv_thread_arg_clear(L, &asarg->targ, LUVF_THREAD_SIDE_CHILD);
+    asarg->L = NULL;
+  }
+  n = luv_thread_arg_set(L, &asarg->targ, 2, lua_gettop(L), LUVF_THREAD_MODE_ASYNC|LUVF_THREAD_SIDE_CHILD);
+  if (n >= 0) {
+    asarg->L = L;
+  }
+  uv_mutex_unlock(argmutex);
+  if (n < 0) {
+    return luv_thread_arg_error(L);
+  }
   ret = uv_async_send(handle);
-  luv_thread_arg_clear(L, arg, LUVF_THREAD_SIDE_CHILD);
+  // clear on gc
   return luv_result(L, ret);
 }
 
