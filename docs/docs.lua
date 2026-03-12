8ce232be3534d7e962b3940712235a31ee5419c6
nilfinx <nulflox@disroot.org>
2026-03-12 11:50:29 +0900

    Use doc.lua instead, refresh with docgen

--- docs/docs.lua
+++ docs/docs.lua
@@ -470,7 +470,7 @@ local doc = {
             local uv = require("luv") -- "luv" when stand-alone, "uv" in luvi apps
 
             local server = uv.new_tcp()
-            server:bind("127.0.0.1", 1337)
+            assert(server:bind("127.0.0.1", 1337))
             server:listen(128, function (err)
               assert(not err, err)
               local client = uv.new_tcp()
